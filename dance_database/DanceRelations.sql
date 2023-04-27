1fe69387e57781d3d3692c7e91bb51daba73592a
Aryan Sharma <131610146+Aryan-sharma-123@users.noreply.github.com>
2023-04-28 01:02:08 +0530

    Entries for the database

---
+++ dance_database/DanceRelations.sql
@@ -0,0 +1,13 @@
+delete from locations;
+
+insert into locations values(1,'Mumbai');
+insert into locations values(2,'Jammu');
+insert into locations values(3,'Bangalore');
+insert into locations values(4,'Delhi');
+insert into locations values(5,'Chennai');
+insert into locations values(6,'Hyderabad');
+insert into locations values(7,'Ahmedabad');
+insert into locations values(8,'Pune');
+insert into locations values(9,'Surat');
+insert into locations values(10,'Jaipur');
+insert into locations values(11,'Kolkata');
